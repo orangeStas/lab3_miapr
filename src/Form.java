3afb7fb09ca6bdafe92f4eb572519a54e05f18b1
orangeStas <orange.css@yandex.by>
2016-02-27 21:39:14 +0300

    Initial commit

---
+++ src/Form.java
@@ -0,0 +1,176 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class Form extends JFrame {
+    private JTextField pC1TextField;
+    private JTextField pC2TextField;
+    private JButton calculateButt;
+    private JPanel rootPanel;
+    private JPanel drawPanel;
+    private JTextField chanceOfFakeAlertField;
+    private JTextField chanceOfSkippingDetectionField;
+    private JTextField summaryErrorOfClassificationField;
+    private JLabel label2;
+    private JLabel label3;
+
+    private final int countPoints = 10000;
+
+    public Form() {
+        super("Lab 3");
+        setContentPane(rootPanel);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        calculateButt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calculate();
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        drawPanel = new JPanel();
+        drawPanel.setLayout(new BorderLayout());
+    }
+
+    private void calculate() {
+        double pc1 = Double.parseDouble(pC1TextField.getText());
+        double pc2 = Double.parseDouble(pC2TextField.getText());
+
+        if (validateParams(pc1, pc2)) {
+            Random random = new Random();
+
+            int[] points1 = new int[countPoints];
+            int[] points2 = new int[countPoints];
+            double mx1 = 0;
+            double mx2 = 0;
+
+            for (int i = 0 ; i < countPoints; i++) {
+                points1[i] = random.nextInt(740 - 100) + 100;
+                points2[i] = random.nextInt(540 + 100) - 100;
+                mx1 += points1[i];
+                mx2 += points2[i];
+            }
+
+            mx1 /= countPoints;
+            mx2 /= countPoints;
+
+            double sigma1 = 0;
+            double sigma2 = 0;
+
+            for (int i = 0 ; i < countPoints; i++) {
+                sigma1 += Math.pow(points1[i] - mx1, 2);
+                sigma2 += Math.pow(points2[i] - mx2, 2);
+            }
+
+            sigma1 = Math.sqrt(sigma1/countPoints);
+            sigma2 = Math.sqrt(sigma2/countPoints);
+
+            double[] result1 = new double[800];
+            double[] result2 = new double[800];
+            result1[0] = (Math.exp(-0.5 * Math.pow((-100 - mx1) / sigma1, 2)) /
+                    (sigma1 * Math.sqrt(2 * Math.PI)) * pc1);
+
+            result2[0] = (Math.exp(-0.5 * Math.pow((-100 - mx2) / sigma2, 2)) /
+                            (sigma2 * Math.sqrt(2 * Math.PI)) * pc2);
+
+
+            int D = 0;
+
+            XYSeries series1 = new XYSeries("P(c1)");
+            XYSeries series2 = new XYSeries("P(c2)");
+
+            for (int x = 1; x < 800; x++) {
+                result1[x] =
+                        (Math.exp(-0.5*Math.pow((x-100 - mx1)/sigma1, 2))/
+                                (sigma1*Math.sqrt(2*Math.PI))*pc1);
+
+                result2[x] =
+                        (Math.exp(-0.5*Math.pow((x-100 - mx2)/sigma2, 2))/
+                                (sigma2*Math.sqrt(2 * Math.PI))*pc2);
+
+                if (Math.abs(result1[x] * 500 - result2[x] * 500) < 0.002)
+                {
+                    D = x;
+                }
+
+                series1.add(x-1, 400 - (int)(result1[x-1]*400*500));
+                series1.add(x, (400 - (int)(result1[x] * 400 * 500)));
+
+                series2.add(x-1, 400 - (int)(result2[x-1]*400*500));
+                series2.add(x, (400 - (int)(result2[x] * 400 * 500)));
+            }
+
+            double error1 = 0;
+            for (int i = 0 ; i < D; i++) {
+                error1 += result2[i];
+            }
+            double error2 = 0;
+            if (pc1 > pc2) {
+                for (int i = D; i < 800; i++) {
+                    error2 += result2[i];
+                }
+            }
+            else {
+                for (int i = D; i < 800; i++) {
+                    error2 += result1[i];
+                }
+            }
+
+            XYSeries crossLine = new XYSeries("");
+            crossLine.add(D, 0);
+            crossLine.add(D, 500);
+
+            chanceOfFakeAlertField.setText(String.valueOf(error1));
+            chanceOfSkippingDetectionField.setText(String.valueOf(error2));
+            summaryErrorOfClassificationField.setText(String.valueOf(error1 + error2));
+
+            final XYSeriesCollection data = new XYSeriesCollection();
+            data.addSeries(series1);
+            data.addSeries(series2);
+            data.addSeries(crossLine);
+            final JFreeChart chart = ChartFactory.createXYLineChart(
+                    "Density distribution",
+                    "X",
+                    "Y",
+                    data,
+                    PlotOrientation.VERTICAL,
+                    true,
+                    true,
+                    false
+            );
+
+            final ChartPanel chartPanel = new ChartPanel(chart);
+            chartPanel.setPreferredSize(new Dimension(800, 400));
+            chartPanel.getChart().getXYPlot().getRangeAxis().setInverted(true);
+            drawPanel.add(chartPanel, BorderLayout.CENTER);
+            drawPanel.validate();
+        }
+    }
+
+    private boolean validateParams(double pc1, double pc2) {
+        if (pc1 > 1 || pc1 < 0) {
+            return false;
+        }
+        if (pc2 > 1 || pc2 < 0) {
+            return false;
+        }
+
+        if (pc1 + pc2 != 1) {
+            return false;
+        }
+        return true;
+    }
+}
